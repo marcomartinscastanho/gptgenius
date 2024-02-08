a50a345620c8d9328168a7c7ac57de1574b79245
Marco Castanho <marco.castanho@toptal.com>
2024-02-08 22:50:03 +0000

    65. member profile

--- components/MemberProfile.tsx
+++ components/MemberProfile.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { UserButton, currentUser } from "@clerk/nextjs";
 
-const MemberProfile = () => {
-  return <div>MemberProfile</div>;
+const MemberProfile = async () => {
+  const user = await currentUser();
+
+  if (user) {
+    return (
+      <div className="px-4 flex items-center gap-2">
+        <UserButton afterSignOutUrl="/" />
+        <p>{user.emailAddresses[0].emailAddress}</p>
+      </div>
+    );
+  }
 };
 
 export default MemberProfile;
