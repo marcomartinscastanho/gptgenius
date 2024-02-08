571cd5e3ca29ad88c340e482df6859188572fc17
Marco Castanho <marco.castanho@toptal.com>
2024-02-08 23:05:12 +0000

    66. theme toggle

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+
+const themes = {
+  light: "fantasy",
+  dark: "dracula",
+};
 
 const ThemeToggle = () => {
-  return <button className="btn btn-primary btn-sm">ThemeToggle</button>;
+  const [theme, setTheme] = useState(themes.light);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === themes.light ? themes.dark : themes.light;
+      document.documentElement.setAttribute("data-theme", newTheme);
+      return newTheme;
+    });
+  };
+
+  return (
+    <button className="btn btn-sm btn-outline" onClick={toggleTheme}>
+      {theme === themes.light ? (
+        <BsMoonFill className="h-4 w-4 " />
+      ) : (
+        <BsSunFill className="h-4 w-4" />
+      )}
+    </button>
+  );
 };
 
 export default ThemeToggle;
