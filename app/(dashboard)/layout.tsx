fcd020aeae87209820c1de68f851a90cad476303
Marco Castanho <marco.castanho@toptal.com>
2024-02-08 22:31:07 +0000

    61. dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
+import Sidebar from "@/components/Sidebar";
 import React, { FC, ReactNode } from "react";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 interface Props {
   children: ReactNode;
@@ -6,9 +8,25 @@ interface Props {
 
 const DashboardLayout: FC<Props> = ({ children }) => {
   return (
-    <div>
-      DashboardLayout
-      {children}
+    <div className="drawer lg:drawer-open">
+      <input type="checkbox" id="my-drawer-2" className="drawer-toggle" />
+      <div className="drawer-content">
+        <label
+          htmlFor="my-drawer-2"
+          className="drawer-button lg:hidden fixed top-6 right-6"
+        >
+          <FaBarsStaggered className="w-8 h-8 text-primary" />
+        </label>
+        <div className="bg-base-200 px-8 py-12 min-h-screen">{children}</div>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        />
+        <Sidebar />
+      </div>
     </div>
   );
 };
