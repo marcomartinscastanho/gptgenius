be6e3983c5f9ecc496afacb21c97e6db83848696
Marco Castanho <marco.castanho@toptal.com>
2024-02-09 09:48:25 +0000

    68. user profile

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { UserProfile } from "@clerk/nextjs";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return <UserProfile />;
 };
 
 export default ProfilePage;
